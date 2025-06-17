74f6b16c30d9e112334c154ebd4dacbc6b8534b5
AnmarSammour <anmarsammour2000@gmail.com>
2025-06-17 15:56:19 +0300

    feat(App): integrate Header component with scroll functionality and remove default content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header/Header";
+
+
+import "./App.module.css";
 
 function App() {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header onNavigate={scrollToSection} />
+      </div>
+    </Router>
   );
 }
 
